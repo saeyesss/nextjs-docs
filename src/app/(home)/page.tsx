409d4a172a05a6c973d0b45e535fdef2427f6071
saeyesss <shreyasnidugala@gmail.com>
2025-01-03 09:07:42 -0700

    add db

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,8 +1,16 @@
-import Link from 'next/link';
+'use client';
+
+import { useQuery } from 'convex/react';
+
 import Navbar from './navbar';
 import TemplateGallery from './template-gallery';
+import { api } from '../../../convex/_generated/api';
 
 const Home = () => {
+  const documents = useQuery(api.documents.get);
+  if (documents === undefined) {
+    return <p>Loading...</p>;
+  }
   return (
     <div className='flex min-h-screen flex-col'>
       <div className='fixed top-0 left-0 right-0 z-10 h-16 p-4 bg-white'>
@@ -10,6 +18,9 @@ const Home = () => {
       </div>
       <div className='mt-16'>
         <TemplateGallery />
+        {documents?.map((document) => (
+          <span key={document._id}>{document.title}</span>
+        ))}
       </div>
     </div>
   );
